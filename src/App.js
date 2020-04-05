678e809038bf4b05313ee3c0188d7a64046be141
Shuyun Yuan <yuanshuyun1012@gmail.com>
2020-04-04 22:47:16 -0700

    Add sign-in card.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { green } from '@material-ui/core/colors';
+import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import SignInCard from './components/SignInCard';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: green[500],
+        },
+        secondary: {
+            main: green[500],
+        },
+        contrastThreshold: 0,
+    },
+});
+
+const useStyles = makeStyles({
+    root: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: green[500],
+    },
+    card: {
+        width: 400,
+        margin: 24,
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <ThemeProvider theme={theme}>
+            <div className={classes.root}>
+                <SignInCard className={classes.card}/>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
